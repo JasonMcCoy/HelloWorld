be1e120d63d542308d7a0f46c6508af86720b097
Jason McCoy <jasonmccoy@Jasons-MacBook-Pro.local>
2016-11-21 19:26:55 -0800

    Hello world in C#
    Simple Hello world in C# project in Visual Studio for Mac

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string firstName = "Jason";
+		string lastName = "McCoy";
+
+		Console.WriteLine(firstName + lastName);
+	}
+}
