d5bf2856030663a4f2d28dd0c498d0370342fced
Jason McCoy <jasonmccoy@Jasons-MacBook-Pro.local>
2016-12-05 04:11:27 -0800

    Fun with if statements
    Having some fun with if statements.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,77 +1,43 @@
 ﻿using System;
-using HelloWorld.Math;
 
-namespace learningCSharp.Math
+namespace learningCSharp
 {
-	public enum shippingMethods
+
+	public class Program
 	{
 
-		twoDayShipping = 2,
-		threeDayShipping = 3,
-		groundShipping = 5
-
-	}
-
-	class Program
-	{
 		static void Main(string[] args)
 		{
-			var Jason = new Person();
 
-			Jason.firstName = "Jason";
-			Jason.lastName = "McCoy";
-
-			Jason.Introduce();
-
-			var calculator = new Calculator();
-			var total = calculator.Add(1, 2);
-
-			Console.WriteLine(total);
+			var Autumn = 80; // 70 - 80 degrees
+			var Spring = 69; // 50 - 69 degrees
+			var Summer = 101; // 101 or higher
+			var Winter = 10; // 10 or less than 10 degrees
 
 
-			// Arrays using integers
-			var numbers = new int[3];
-			numbers[0] = 1;
-			numbers[1] = 2;
-			numbers[2] = 3;
-
-			Console.WriteLine(numbers[0]);
-			Console.WriteLine(numbers[1]);
-			Console.WriteLine(numbers[2]);
-
-
-			// Arrays using strings
-			var names = new string[3];
-			names[0] = "Jason";
-			names[1] = "Irene";
-			names[2] = "Josh";
-
-			Console.WriteLine(names[0]);
-			Console.WriteLine(names[1]);
-			Console.WriteLine(names[2]);
-
-
-			// Verbatim string
-			var someText = @"Hi x, please do the following tasks today:
-			1. Work on x
-            		2. Then work on y
-			3. Then finish with z";
-
-			Console.WriteLine(someText);
-
-			var firstName = "Jason";
-			var lastName = "McCoy";
-
-			var myFullName = string.Format("My full name is {0}, {1}", firstName, lastName);
-			Console.WriteLine(myFullName);
-
-
-			var twoDayShipping = shippingMethods.twoDayShipping;
-			var threeDayShipping = shippingMethods.threeDayShipping;
-			var fiveDayShipping = shippingMethods.groundShipping;       
-
-			Console.WriteLine("Hi, your shipment will arrive in {0} days.", (int)twoDayShipping);
-
+			if (Autumn >= 70 && Autumn <= 80)
+			{
+				Console.WriteLine("It's Autumn!!!");
+			}
+			else if (Spring >= 50 && Spring < 69)
+			{
+				Console.WriteLine("It's Spring!!!");
+			}
+			else if (Summer >= 101)
+			{
+				Console.WriteLine("It's Summer!!!");
+			}
+			else if (Winter <= 10)
+			{
+				Console.WriteLine("It's Winter!!!");
+			}
+			else
+			{
+				Console.WriteLine("No season detected.");
+			}
+		
 		}
+	
 	}
+
 }
---
+++ HelloWorld/Season.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace learningCSharp
+{
+	public class Season
+	{
+
+		var Spring = 69; // 50 - 69 degrees
+		var Summer = 101; // 101 or higher
+		var Autumn = 80; // 70 - 80 degrees
+		var Winter = 10; // 10 or less than 10 degrees
+
+	}
+}
